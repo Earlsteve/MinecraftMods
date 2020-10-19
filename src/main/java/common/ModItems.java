02e25fc93aa338727049ca74ecb2a752c10a6831
Pixelherz23 <64771916+Pixelherz23@users.noreply.github.com>
2020-10-19 09:50:32 +0200

    define group

--- src/main/java/common/ModItems.java
+++ src/main/java/common/ModItems.java
@@ -1,4 +1,14 @@
 package common;
 
+import client.titan.Registration;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+
 public class ModItems {
+    public static final RegistryObject<Item> TITAN_INGOT = Registration.ITEMS.register("titan_ingot",
+            () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS)));
+    
+
 }
