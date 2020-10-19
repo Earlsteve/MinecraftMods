fc63231a63d2ab09d036b596d15636dbcb35396b
Nils <nils-jollet@gmx.de>
2020-10-19 11:32:00 +0200

    Mod Tiers

--- src/main/java/common/ModItems.java
+++ src/main/java/common/ModItems.java
@@ -1,8 +1,10 @@
 package common;
 
 import client.titan.Registration;
+import net.minecraft.item.IItemTier;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SwordItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 
@@ -10,6 +12,8 @@ public class ModItems {
     public static final RegistryObject<Item> TITAN_INGOT = Registration.ITEMS.register("titan_ingot",
             () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS)));
 
+    public static final RegistryObject<Item> TITAN_SWORD = Registration.ITEMS.register("titan_sword",
+            () -> new SwordItem();
   //nothing in it only for "classloading"
 public static void register(){
 
